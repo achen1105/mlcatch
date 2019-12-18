f9a80d4121dfc999f46770b50a83a89681d4d65c
Anita Chen <achen1105@gmail.com>
2019-12-18 01:37:53 -0800

    Added instructions, about, game screens, and transitions.

--- scripts/catchmain.js
+++
@@ -1,9 +0,0 @@
-// ahhhh
-
-let myButton = document.querySelector('button');
-
-function setUserName() {
-  let myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla is cool, ' + myName;
-}
